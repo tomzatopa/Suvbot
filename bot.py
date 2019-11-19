c778c6f8031182debb98041bebb4a76aff96d4a8
Pavel Specian <speecik@gmail.com>
2019-11-19 23:23:32 +0100

    uprava na bot commands

--- bot.py
+++ bot.py
@@ -2,34 +2,21 @@ import os
 import random
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix='!')
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
 #vyber random radku z filu - Ehrendil
 def rand_line(soubor):
     x = random.choice(list(open(soubor)))
     return x
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(
-        f'{client.user} is connected to the following server:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-#zprehledneni promennych - Ehrendil
+@bot.command(name='leaveguld', help='!leaveguld osoba1 osoba2')
+async def leaveguld(ctx, arg1, arg2):
+    osoba1 = str(arg1)
+    osoba2 = str(arg2)
     pridjm = str(rand_line('pridjm.txt')).rstrip()
     nad = str(rand_line('nad.txt')).rstrip()
     nade = str(rand_line('nade.txt')).rstrip()
@@ -40,25 +27,25 @@ async def on_message(message):
     elgee = str(rand_line('elgee.txt')).rstrip()
     guilda = str(rand_line('guilda.txt')).rstrip()
 
-    if message.content == '!leaveguild':
-#leave template
-        leave='Ahoj, rozhodl jsem se leavnout guildu, protože Lesi je ' + nad \
-            + ' a ' + pridjm \
-            + ' ' + nad \
-            + ', který ' + lesi \
-            + '. Hraju to už '+str(random.randrange(5,51)) \
-            +' let a prošel jsem už '+str(random.randrange(5,21)) \
-            +' guild a s takovým ' + nadm \
-            + ' jako je Lesi jsem se ještě nesetkal. Doufám, že v příštím tieru ' + guilda \
-            + '. Strčte si vaší guildu do ' + misto \
-            + ', jdu mít '+str(random.randrange(1,51)) \
-            +' parsy jinam! A LG ty ' + pridjm \
-            + ' ' + nade \
-            + ' se taky můžeš ' + elgee \
-            + ' ty ' + nade2+ '!'
-        await message.channel.send(leave)
+    leave='Ahoj, rozhodl jsem se leavnout guildu, protože '+osoba1+' je ' + nad \
+        + ' a ' + pridjm \
+        + ' ' + nad \
+        + ', který ' + lesi \
+        + '. Hraju to už '+str(random.randrange(5,51)) \
+        +' let a prošel jsem už '+str(random.randrange(5,21)) \
+        +' guild a s takovým ' + nadm \
+        + ' jako je '+osoba1+' jsem se ještě nesetkal. Doufám, že v příštím tieru ' + guilda \
+        + '. Strčte si vaší guildu do ' + misto \
+        + ', jdu mít '+str(random.randrange(1,51)) \
+        +' parsy jinam! A '+osoba2+' ty ' + pridjm \
+        + ' ' + nade \
+        + ' se taky můžeš ' + elgee \
+        + ' ty ' + nade2+ '!'
+    await ctx.send(leave)
 
-    elif message.content == 'raise-exception':
-       raise discord.DiscordException
+@leaveguld.error
+async def info_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Je potřeba dodat jmena lidi: osoba1 osoba2')
 
-client.run(TOKEN)
+bot.run(TOKEN)
