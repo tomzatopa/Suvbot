f42bbd672b3528aaba2d907f399eae35922c6c7c
Pavel Specian <speecik@gmail.com>
2019-11-19 21:07:46 +0100

    zprehledneni kodu, nacitani textu ze souboru

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 import os
 import random
-
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+
+#vyber random radku z filu - Ehrendil
+def rand_line(soubor):
+    x = random.choice(list(open(soubor)))
+    return x
 
 client = discord.Client()
 
@@ -25,87 +29,36 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    pridjm = [
-        'dementní',
-        'vyjebanej',
-        'zkurvenej',
-        'zasranej',
-        'vypíčenej',
-        'zpíčenej',
-    ]
-    nad = [
-        'kotot',
-        'zmrd',
-        'čůrák',
-        'dement',
-        'mentál',
-        'fagot',
-    ]
-    nade = [
-        'kotote',
-        'zmrde',
-        'čůráku',
-        'demente',
-        'mentále',
-        'fagote',
-    ]
-    nade2 = [
-        'kotote',
-        'zmrde',
-        'čůráku',
-        'píčo jedna',
-        'chcanko',
-        'sračko',
-        'mrdko jedna',
-    ]
-    nadm = [
-        'kototem',
-        'zmrdem',
-        'čůrákem',
-        'zkurvysynem',
-
-    ]
-
-    misto = [
-        'prdele',
-        'řiti',
-        'kundy',
-        'píči',
-        'zadku',
-        'víte kam'
-    ]
-    lesi = [
-        'jenom na všechny řve',
-        'akorát každýho kritizuje',
-        'stejně neumí hrát ferála',
-        'akorát parsuje na píču',
-        'si o sobě myslí jak je dobrej, i když je to nula',
-        'ani neumí vyprat polštář',
-    ]
-    elgee = [
-        'jít zabít',
-        'jít vysrat',
-        'KYS',
-        'jít oběsit',
-        'jít zastřelit',
-    ]
-    guilda = [
-        'dostanete cancer',
-        'všichni shoříte',
-        'nezabijete ani prvního bosse na mythic',
-        'nedoděláte ani curvu',
-        'všichni commitnete noliving',
-        'dostanete ban',
-    ]
-
+#zprehledneni promennych - Ehrendil
+    pridjm = str(rand_line('pridjm.txt')).rstrip()
+    nad = str(rand_line('nad.txt')).rstrip()
+    nade = str(rand_line('nade.txt')).rstrip()
+    nade2 = str(rand_line('nade2.txt')).rstrip()
+    nadm = str(rand_line('nadm.txt')).rstrip()
+    misto = str(rand_line('misto.txt')).rstrip()
+    lesi = str(rand_line('lesi.txt')).rstrip()
+    elgee = str(rand_line('elgee.txt')).rstrip()
+    guilda = str(rand_line('guilda.txt')).rstrip()
 
     if message.content == '!leaveguild':
-
-        leave='Ahoj, rozhodl jsem se leavnout guildu, protože Lesi je ' + random.choice(nad) + ' a ' + random.choice(pridjm) + ' ' + random.choice(nad) + ', který ' + random.choice(lesi) + '. Hraju to už '+str(random.randrange(5,51))+' let a prošel jsem už '+str(random.randrange(5,21))+' guild a s takovým ' + random.choice(nadm) + ' jako je Lesi jsem se ještě nesetkal. Doufám, že v příštím tieru ' + random.choice(guilda) + '. Strčte si vaší guildu do ' + random.choice(misto) + ', jdu mít '+str(random.randrange(1,51))+' parsy jinam! A LG ty ' + random.choice(pridjm) + ' ' + random.choice(nade) + ' se taky můžeš ' + random.choice(elgee) + ' ty ' + random.choice(nade2)+ '!'
+#leave template
+        leave='Ahoj, rozhodl jsem se leavnout guildu, protože Lesi je ' + nad \
+            + ' a ' + pridjm \
+            + ' ' + nad \
+            + ', který ' + lesi \
+            + '. Hraju to už '+str(random.randrange(5,51)) \
+            +' let a prošel jsem už '+str(random.randrange(5,21)) \
+            +' guild a s takovým ' + nadm \
+            + ' jako je Lesi jsem se ještě nesetkal. Doufám, že v příštím tieru ' + guilda \
+            + '. Strčte si vaší guildu do ' + misto \
+            + ', jdu mít '+str(random.randrange(1,51)) \
+            +' parsy jinam! A LG ty ' + pridjm \
+            + ' ' + nade \
+            + ' se taky můžeš ' + elgee \
+            + ' ty ' + nade2+ '!'
         await message.channel.send(leave)
 
     elif message.content == 'raise-exception':
        raise discord.DiscordException
-
 
 client.run(TOKEN)
