f7dfd68a6c9a8683d0280bb5d379fbed5d732e98
ehrendil-iao <specik@specik.eu>
2019-11-24 23:27:56 +0100

    test

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ MAINTAINER = [
 ###########EXTENSIONS##########
 ###############################
 bot.load_extension('gamble')
-bot.add_cog(gamble(bot))
+bot.add_cog(Gabmle(bot))
 
 ###############################
 ##########BOT EVENTS###########
