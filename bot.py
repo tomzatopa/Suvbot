d5b2ec9bf218fb074de35829805343a8cb676c39
tomzatopa <tomzatopa@gmail.com>
2020-01-24 18:20:56 +0100

    vicenasobne prehravani iaosound

--- bot.py
+++ bot.py
@@ -402,10 +402,17 @@ async def info_error(ctx, error):
 
 #iaosound command
 @bot.command(name='iaosound')
-async def iaosound(ctx, arg1):
+async def iaosound(ctx, arg1, *args):
     channel = ctx.author.voice.channel
     #checkuje jestli existuje file ve slozce sounds/
     if path.exists('./sounds/'+arg1+'.mp3'):
+        if args and args[0].isnumeric() == True:
+               for x in range(int(args[0])-1):
+                   vc = await channel.connect()
+                   vc.play(discord.FFmpegPCMAudio('./sounds/'+arg1+'.mp3'), after=lambda e: print('prehravam', e))
+                   while vc.is_playing():
+                       await asyncio.sleep(1)
+                   await ctx.voice_client.disconnect()
         vc = await channel.connect()
         vc.play(discord.FFmpegPCMAudio('./sounds/'+arg1+'.mp3'), after=lambda e: print('prehravam', e))
         while vc.is_playing():
