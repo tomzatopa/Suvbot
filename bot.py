335d163fbd050803bc6a9653d67aa404db8467c4
tomzatopa <tomzatopa@gmail.com>
2020-04-21 15:20:37 +0200

    tohle staci jako else at je vic hlupaku

--- bot.py
+++ bot.py
@@ -143,11 +143,11 @@ async def on_message(message):
                 return
             if response=='ano':
                 await user.send("Výborně! Můžeme začít s přihláškou.")
-            if response=='ne':
+            else:
                 await user.send("Tvoje přihláška se ruší. Nemám na to, sorry.")
                 channel=bot.get_channel(702074796984500234)
                 await channel.send("<@&464769766117212160> Ahoj všichni! Rád bych vám oznámil, že <@"+str(id)+"> je debil! HALÓ HALÓ!!! <@"+str(id)+"> JE HLUPÁK!!! Nemám na to s ním vyplňovat přihlášku. Nebudu to dělat...")
-                return
+                return                            
         else:
             await user.send("Hahaha! Napíšu botovi něco jinýho než ano/ne, protože na to beztak nikdo nemyslel? Oooooooo jak originální! Když jsi tak chytrej, tak jdeme vyplňovat přihlášku.")
 
