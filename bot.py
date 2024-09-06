8eb211395e0cddb07681800da5b7569d983640bd
tomzatopa <tomzatopa@gmail.com>
2024-09-07 01:12:56 +0200

    test

--- bot.py
+++ bot.py
@@ -1135,7 +1135,7 @@ async def info_error(ctx, error):
 #luraquote command
 @bot.command(name='luraquote')
 async def luraquote(ctx):
-    quote= str(rand_line('luraquotes.txt')).rstrip()
+    quote= "test" + str(rand_line('luraquotes.txt')).rstrip()
     await ctx.send(quote)
 
 #inspire command - inspirobot
