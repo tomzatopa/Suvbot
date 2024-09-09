a181690ab713f3edf10d1f6b73258cd9b8b4eb13
ihavenolife <matuswall@gmail.com>
2024-09-09 20:09:21 +0200

    quotes thing

--- create_db.py
+++ create_db.py
@@ -7,8 +7,12 @@ import requests
 db_connection = sqlite3.connect("suvbot.db")
 db_cursor = db_connection.cursor()
 
-db_cursor.execute("CREATE TABLE active_invasions(reward1, reward2)")
-db_cursor.execute("CREATE TABLE tracked_rewards(reward)")
+try: db_cursor.execute("CREATE TABLE active_invasions(reward1, reward2)")
+except: pass
+try: db_cursor.execute("CREATE TABLE tracked_rewards(reward)")
+except: pass
+try: db_cursor.execute("CREATE TABLE saved_quotes(guild_id, channel_id, message_id UNIQUE, message_content, locked)")
+except: pass
 
 #db_cursor.execute("INSERT INTO active_invasions (reward1, reward2, planet, mission_type) VALUES ('testval1', 'testval2', 'hirukovamama', 'speedrun');")
 #db_connection.commit()
