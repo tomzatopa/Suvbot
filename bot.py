0a2fada50e26d019c79bf863b3f21d660b977ddf
tomzatopa <tomzatopa@gmail.com>
2025-04-01 17:06:04 +0000

    fix?

--- bot.py
+++ bot.py
@@ -1469,7 +1469,7 @@ async def weeklinka(ctx:commands.Context):
         if ctx.channel.id==798224390579421244:
             tanks = ["161886775000825857", "556539219003047936","171216311362256897","273852645741953024"]
             tank= random.choice(tanks)
-            await ctx.send(f"Pls help s weeklinkou <@{tank}>")
+            await ctx.send("Pls help s weeklinkou <@"+tank+">")
         else:
             await ctx.send("Tady ne")
     else: 
